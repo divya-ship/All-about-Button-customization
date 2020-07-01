1c84ec1f5eab5d087d748d1b262110c6595aa87a
Divya <divyavk279@gmail.com>
2020-07-02 00:28:42 +0530

    First Commit

---
+++ app/src/main/java/com/example/buttoncustomization/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.buttoncustomization;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button=findViewById(R.id.button3);
+        button.setEnabled(false);
+    }
+}
